1583732e9c2d9bf972605837c74fa58f7bd0df27
zxy <1246830115@qq.com>
2017-12-13 21:26:08 +0800

    3

--- src/com/zxy/test/MyTest.java
+++ src/com/zxy/test/MyTest.java
@@ -3,6 +3,7 @@ package com.zxy.test;
 public class MyTest {
 	public static void main(String[] args){
 		System.out.println("hello world");
-		System.out.println("1");
+		System.out.println("2");
+		System.out.println(1);
 	}
 }
